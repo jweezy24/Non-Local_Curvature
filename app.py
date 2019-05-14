ddc26f87c463a1fc4e5acd65016684a7789d0efc
jweezy <jwest1@luc.edu>
2019-05-14 10:40:47 -0500

    simple function parser created

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import integration_lib.parser_west as parser
+import integration_lib.function as func
 
 check_parse = parser.parser()
 
 print(check_parse.args)
+
+test_func = func.math_function(check_parse.args["args"]["function"])
+
+print(test_func.run_func([2,1]))
--- integration_lib/function.py
+++ integration_lib/function.py
@@ -1,6 +1,29 @@
 import parser
+import sys
+
+this_mod = sys.modules[__name__]
 
 class math_function:
-    def __init__(self, str):
 
+    def __init__(self, str):
+        self.vars = []
+        for i in str:
+            if ord(i) >= 97 and ord(i) <= 122:
+                self.vars.append(i)
+            else:
+                continue
         self.func = parser.expr(str).compile()
+
+    def run_func(self, args):
+        if len(args) != len(self.vars):
+            print("need more args")
+            return False
+        else:
+            template = '{0} = {1}\n'
+            strng = ''
+            filename = ''
+            for i in range(0, len(self.vars)):
+                strng += template.format(self.vars[i], args[i])
+            code = compile(strng, filename, 'exec')
+            exec(code)
+            return eval(self.func)
